e0818afd332c9d21936738c2129d9017de1ecff9
Tamira2804 <tamira2804@gmail.com>
2023-09-14 09:55:30 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../../redux/operations';
-// import { selectIsLoading, selectError } from '../../redux/selectors';
 import ContactForm from '../ContactForm';
 import ContactsList from '../ContactsList';
 import Filter from '../Filter';
@@ -9,8 +8,6 @@ import './App.scss';
 
 const App = () => {
   const dispatch = useDispatch();
-  // const isLoading = useSelector(selectIsLoading);
-  // const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -20,7 +17,6 @@ const App = () => {
     <div className="App__container">
       <h1>Phonebook</h1>
       <ContactForm />
-      {/* {isLoading && !error && <b>Request in progress...</b>} */}
       <h2>Contacts</h2>
       <Filter />
       <ContactsList />
